bf281aa302e006b48b78e5fa5b84f4210fafc8b0
root <root@DESKTOP-RQD2OCE.localdomain>
2021-05-19 19:17:58 -0400

    As far as we got

--- index.js
+++ index.js
@@ -61,6 +61,48 @@ app.post('/create', (req, res) => {
   }
 })
 
+app.get('/create-interesting', csrfProtection, (req, res) => {
+  res.render('create-interesting', {csrfToken: req.csrfToken()})
+})
+
+app.post('/create-interesting', csrfProtection, (req, res) => {
+  const { firstName, lastName, email, password, confirmedPassword, age, favoriteBeatle, iceCream } = req.body;
+  const errors= [];
+  if (!firstName){
+    errors.push("Please provide a first name.")
+  }
+  if (!lastName){
+    errors.push("Please provide a last name.")
+  }
+  if (!email){
+    errors.push("Please provide an email.")
+  }
+  if (!password){
+    errors.push("Please provide a password.")
+  } else if(password !== confirmedPassword) {
+    errors.push("The provided values for the password and password confirmation fields did not match.")
+  }
+  if(!age){
+    errors.push('age is required');
+  } else {
+    if(typeof age !== number){
+      errors.push('age must be a valid age');
+    }
+  }
+  if (errors.length > 0){
+    res.render("create-interesting", {errors, firstName, lastName, email})
+  } else{
+  const user = {
+    id: users.length + 1,
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+  }
+  users.push(user);
+  res.redirect('/');
+  }
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
